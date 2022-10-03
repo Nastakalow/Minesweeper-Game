09e78a837183e1a536f24d9dd28b34be575a0367
Ramazan Nastakalov <nastakalow@gmail.com>
2022-10-03 21:58:59 +0400

    70% complete

--- js/app.js
+++ js/app.js
@@ -1,29 +1,129 @@
-// class MineSweeper {
-//   qutular = document.querySelectorAll("#boxes .box");
-//   minalar = [];
-//   oyunBitib = false;
-
-//   constructor() {
-//     this.minalariDuz();
-//     console.log(this.minalar);
-//     this.minalarinSayiniGoster();
-//     this.qutularaClickleriYapishdir();
-//   }
-
-//   minalariDuz() {
-//     for (let i = 0; i < this.qutular.length; i++) {}
-//   }
-// }
-
+const boxes = document.querySelectorAll("#boxes .box");
 const flag = document.querySelector("#flag");
 const mine = document.querySelector("#mine");
+const flagCount = document.querySelector("#flagCount");
+const timeCount = document.querySelector("#timeCount");
+const resetBtn = document.querySelector("#reset-btn");
 
-flag.addEventListener("click", () => {
+let int = null;
+let seconds = 0;
+let mines = [];
+
+flag.addEventListener("click", switchFlag);
+mine.addEventListener("click", switchMine);
+resetBtn.addEventListener("click", resetGame);
+boxes.forEach((box) =>
+  box.addEventListener("click", () => {
+    startTime();
+  })
+);
+
+function startTime() {
+  if (int !== null) {
+    clearInterval(int);
+  }
+  int = setInterval(countSecond, 1000);
+
+  function countSecond() {
+    seconds++;
+
+    if (seconds == 999) {
+      seconds = 0;
+    }
+
+    let s =
+      seconds >= 100
+        ? "" + seconds
+        : seconds >= 10
+        ? "0" + seconds
+        : "00" + seconds;
+
+    timeCount.innerHTML = s;
+  }
+}
+
+function resetGame() {
+  clearInterval(int);
+  timeCount.innerHTML = "000";
+}
+
+function switchFlag() {
   flag.classList.add("active");
   mine.classList.remove("active");
-});
+}
 
-mine.addEventListener("click", () => {
+function switchMine() {
   mine.classList.add("active");
   flag.classList.remove("active");
-});
+}
+
+function setMines() {
+  for (let i = 0; i < boxes.length; i++) {
+    let randomMine = Math.round(Math.random()) == 1;
+    mines.push(randomMine);
+  }
+  return mines;
+}
+
+function findMinesCount() {
+  let count = 0;
+  for (i of mines) {
+    if (i) {
+      count++;
+    }
+  }
+  flagCount.innerHTML = count;
+}
+
+function setMinesCount() {
+  let count;
+  let minesCount = [];
+  for (let i = 0; i < boxes.length; i++) {
+    count = 0;
+    if (!mines[i]) {
+      for (let j = 1; j <= 6; j++) {
+        if (j != 2 && j != 3) {
+          if (i % 5 == 0) {
+            if (j != 6 && j != 1) {
+              if (mines[i - j]) {
+                count++;
+              }
+            }
+            if (j != 4) {
+              if (mines[i + j]) {
+                count++;
+              }
+            }
+          } else if (i % 5 == 4) {
+            if (j != 4 && j != 1) {
+              if (mines[i - j]) {
+                count++;
+              }
+            }
+
+            if (j != 6) {
+              if (mines[i + j]) {
+                count++;
+              }
+            }
+          } else {
+            if (mines[i - j]) {
+              count++;
+            }
+
+            if (mines[i + j]) {
+              count++;
+            }
+          }
+        }
+      }
+      boxes[i].innerHTML = count;
+      minesCount.push(count);
+    }
+  }
+  return minesCount;
+}
+
+console.log(setMines());
+console.log(setMinesCount());
+console.log(findMinesCount());
